aa551f318dfedd661048ba49b12de260e260979f
Jade Hatchi - Pecqueux <jhatchi-@paul-f4Br1s1.clusters.42paris.fr>
2025-03-07 18:03:55 +0100

    cpp 00 premier push

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		std::cout << "* AAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHH *";
+	}
+	else 
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	return 0;
+}
