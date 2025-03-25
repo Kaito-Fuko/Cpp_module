881116488c5f6835b25d6a2d77525fdd7c485a67
Jade Hatchi - Pecqueux <jhatchi-@paul-f4Br3s4.clusters.42paris.fr>
2025-03-25 15:14:28 +0100

    cpp 02 ex00 fait ex01 fait

---
+++ cpp_02/ex01/include/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class	Fixed
+{
+	private:
+		int 				value;
+		static const int	valueBits = 8;
+
+	public:
+		Fixed();
+		Fixed(int value);
+		Fixed(float value);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		friend std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
+		~Fixed();
+
+		int		getRawBits() const;
+		void	setRawBits(int value);
+		int		toInt() const;
+};
+
+#endif
