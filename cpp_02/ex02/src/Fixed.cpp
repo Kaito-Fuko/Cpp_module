ee68b592c75f3605b6c5772fdc4ad31525c1dea2
Jade Hatchi - Pecqueux <jhatchi-@paul-f4Br2s1.clusters.42paris.fr>
2025-03-27 14:07:24 +0100

    cpp 02 ex00 fait ex01 fait ex02 fait ex03 en cour mais je vais pas le finir ni le push

--- cpp_02/ex02/src/Fixed.cpp
+++ cpp_02/ex02/src/Fixed.cpp
@@ -2,28 +2,19 @@
 #include "../include/Fixed.hpp"
 
 Fixed::Fixed(): value(0)
-{
-	// std::cout << "Default constructor called" << std::endl;
-}
+{}
 
 Fixed::Fixed(int value): value(value << valueBits)
-{
-	// std::cout << "int constructor called" << std::endl;
-}
+{}
 
 Fixed::Fixed(float value): value(roundf(value * (1 << valueBits)))
-{
-	// std::cout << "float constructor called" << std::endl;
-}
+{}
 
 Fixed::Fixed(const Fixed& other): value(other.value)
-{
-	// std::cout << "Copy constructor called" << std::endl;
-}
+{}
 
 Fixed&	Fixed::operator=(const Fixed& other)
 {
-	// std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		this->value = other.value;
 	return *this;
