0f2d391300209c140811cf13dc72a752f4bfdfc5
Jade Hatchi - Pecqueux <jhatchi-@paul-f4Br7s4.clusters.42paris.fr>
2025-03-31 14:26:38 +0200

    cpp 01 ex06 changement des message

--- cpp_01/ex06/include/Harl.hpp
+++ cpp_01/ex06/include/Harl.hpp
@@ -14,7 +14,7 @@ class Harl
     public:
         Harl();
         ~Harl();
-        void    complain(std::string level);
+        // void    complain(std::string level);
         void    filter(std::string level);
 };
 
--- cpp_01/ex06/src/Harl.cpp
+++ cpp_01/ex06/src/Harl.cpp
@@ -8,35 +8,35 @@ Harl::~Harl()
 
 void    Harl::error()
 {
-    std::cout << "error" << std::endl;
+    std::cout << "Error: brain 404." << std::endl;
 }
 
 void    Harl::warning()
 {
-    std::cout << "warning" << std::endl;
+    std::cout << "Warning: brain not found." << std::endl;
 }
 
 void    Harl::info()
 {
-    std::cout << "info" << std::endl;
+    std::cout << "Info: the brain is pink because the blood is red." << std::endl;
 }
 
 void    Harl::debug()
 {
-    std::cout << "debug" << std::endl;
+    std::cout << "Debug: a brain is necessary for this complaint." << std::endl;
 }
 
-void    Harl::complain(std::string level)
-{
-    void    (Harl::*function[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+// void    Harl::complain(std::string level)
+// {
+//     void    (Harl::*function[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+//     std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-    for (int i = 0; i < 4; i++)
-        if (level == levels[i])
-            return (this->*function[i])();
+//     for (int i = 0; i < 4; i++)
+//         if (level == levels[i])
+//             return (this->*function[i])();
 
-    std::cerr << "error: " << level << " doesn't exists" << std::endl;
-}
+//     std::cerr << "Error: " << level << " doesn't exists." << std::endl;
+// }
 
 void Harl::filter( std::string level ) {
     std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
