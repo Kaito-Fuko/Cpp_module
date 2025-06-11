a0d0a751fff196258e83e55b00876f9631772bcd
Jade Hatchi - Pecqueux <jhatchi-@f2r2s17.clusters.42paris.fr>
2025-06-11 14:16:47 +0200

    cpp 09 ex00 en cour

--- cpp_09/ex00/src/BitcoinExchange.cpp
+++ cpp_09/ex00/src/BitcoinExchange.cpp
@@ -32,7 +32,7 @@ BitcoinExchange::BitcoinExchange()
 	std::ifstream file("data.csv");
 	if (!file)
 	{
-		std::cout << "Error: invalid file." << std::endl;
+		std::cout << RED "Error: Invalid file." END << std::endl;
 		return ;
 	}
 
@@ -71,11 +71,18 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange & other)
 BitcoinExchange::~BitcoinExchange()
 {}
 
-int check(std::string& date)
+int checkDate(std::string& date, int i)
 {
+	if (date.empty())
+	{
+		std::cout << RED "Error404: Date not found." << " (line " << i << ")" << END << std::endl;
+		return 1;
+	}
+
 	if (date.size() != 10 || std::count(date.begin(), date.end(), '-') != 2)
 	{
-		std::cout << RED "Error: Wrong format: " << BOLD << date << RED " or I want YYYY-MM-DD" END << std::endl;
+		std::cout << RED "Error: Wrong format: " << BOLD << date
+			<< RED " or I want YYYY-MM-DD." << " (line " << i << ")" << END << std::endl;
 		return 1;
 	}
 
@@ -85,15 +92,15 @@ int check(std::string& date)
 
 	if (year < 0)
 	{
-		std::cout << RED "Error: invalid year: " 
-			<< BOLD << year << RED "-" << month << "-" << day << END << std::endl;
+		std::cout << RED "Error: Invalid year: " 
+			<< BOLD << year << RED "-" << month << "-" << day << ". (line " << i << ")" << END << std::endl;
 		return 1;
 	}
 
 	if (month < 1 || month > 12)
 	{
-		std::cout << RED "Error: invalid month: " 
-			<< year << "-" << BOLD << month << RED "-" << day << END << std::endl;
+		std::cout << RED "Error: Invalid month: " 
+			<< year << "-" << BOLD << month << RED "-" << day << ". (line " << i << ")" << END << std::endl;
 		return 1;
 	}
 
@@ -107,20 +114,41 @@ int check(std::string& date)
 
     if (day < 1 || day > daysInMonth[month - 1])
     {
-        std::cerr << RED "Error: invaid day: " 
-			<< year << "-" << month << "-" << BOLD << day << END << std::endl;
+        std::cerr << RED "Error: Invaid day: " 
+			<< year << "-" << month << "-" << BOLD << day << RED ". (line " << i << ")" << END << std::endl;
 		return 1;
     }
 
 	return 0;
 }
 
+int checkValue(std::string value, int i)
+{
+	if (value.empty())
+	{
+		std::cout << RED "Error404: Value not found." << " (line " << i << ")" << END << std::endl;
+		return 1;
+	}
+	if (toDouble(value) > 1000 || toInt(value) < 0)
+	{
+		std::cout << RED "Error: The number must be between 0 and 1000. Value: " BOLD
+			<< value << RED ". (line " << i << ")" << END << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
 void	BitcoinExchange::convert(char* str)
 {
+	if (_data.empty())
+	{
+		std::cout << RED "Error404: Data not found." END << std::endl;
+		return ;
+	}
 	std::ifstream file(str);
 	if (!file)
 	{
-		std::cout << RED "Error: invalid file." END << std::endl;
+		std::cout << RED "Error: Invalid file." END << std::endl;
 		return ;
 	}
 
@@ -134,31 +162,42 @@ void	BitcoinExchange::convert(char* str)
 			i++;
 			continue;
 		}
+		i++;
+		if (line.empty())
+		{
+			std::cout << "empty line. (line " << i << ")." << std::endl;
+		}
 		std::istringstream iss(line);
 		std::string date;
 		std::string value;
+
+		if (std::count(line.begin(), line.end(), '|' ) != 1)
+		{
+			std::cout << RED "Error: Invalid Format. (line " << i << ")" END << std::endl;
+			continue;
+		}
+
 		if (std::getline(iss, date, '|') && std::getline(iss, value))
 		{
 			date.erase(0, date.find_first_not_of(" \t"));
 			date.erase(date.find_last_not_of(" \t\r\n") + 1);
 			value.erase(0, value.find_first_not_of(" \t"));
 			value.erase(value.find_last_not_of(" \t\r\n") + 1);
-			if (check(date))
-			{
-				continue;
-			}
 			
-			if (toDouble(value) > 1000 || toInt(value) < 0)
-			{
-				std::cout << RED "Error: invalid number: " << date << " | " << BOLD << value << END << std::endl;
+			if (checkDate(date, i))
 				continue;
-			}
+			
+			if (checkValue(value, i))
+				continue;
 
 			if (!_data[date])
 			{
-				std::cout << "a faire ";
+				std::cout << "A faire ";
 			}
 			std::cout << date << " => " << _data[date] << " = " << _data[date] * toDouble(value) << std::endl;
 		}
+		if (value.empty())
+			std::cout << RED "Error404: Value not found." << " (line " << i << ")"
+				<< END << std::endl;
 	}
 }
